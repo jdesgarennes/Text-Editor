30d5f5e99884e40701b244a9190a806cd85864f7
jdesgarennes <johndesgarennes@gmail.com>
2022-01-08 18:02:36 -0800

    cleand up some comments

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -22,7 +22,30 @@ module.exports = () => {
         template: './index.html',
         title: 'Just Another Text Editor'
       }),
+
+// Creates a manifest.json file.
+new WebpackPwaManifest({
+  fingerprints: false,
+  inject: true,
+  name: 'Just Another Text Editor',
+  short_name: 'Contact',
+  description: 'Just Another Text Editor!',
+  background_color: '#225ca3',
+  theme_color: '#225ca3',
+  start_url: '/',
+  publicPath: '/',
+  icons: [
+    {
+      src: path.resolve('src/images/logo.png'),
+      sizes: [96, 128, 192, 256, 384, 512],
+      destination: path.join('assets', 'icons'),
+    },
+  ],
+}),
+
     ],
+
+    
 
     module: {
       rules: [
